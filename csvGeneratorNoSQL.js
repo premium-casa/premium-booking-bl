32daab6ee6023bc4a4fd39b0fc468c182399449a
bjlagman12 <bjlagman@gmail.com>
2019-06-07 00:58:20 -0700

    Set up generator for noSQL

--- csvGeneratorNoSQL.js
+++ csvGeneratorNoSQL.js
@@ -1,62 +1,65 @@
-/* eslint-disable quotes */
-/* eslint-disable no-console */
 /* eslint-disable comma-dangle */
-/* eslint-disable operator-linebreak */
-/* eslint-disable prefer-template */
+/* eslint-disable no-await-in-loop */
 const fs = require('fs');
-const faker = require('faker');
 const moment = require('moment');
 
 const randomNum = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
-const roomNameAppendix = ['\'s Apartment', '\'s House', '\'s Loft', '\'s Condo'];
-const day = 86400000;
+const roomNameAppendix = ['s Apartment', 's House', 's Loft', 's Condo'];
+const name = ['tom gay', 'sam hank', 'bill yee', 'liz harry', 'ashley nguyen', 'michael scott', 'pam beesly'];
 
+const randomName = () => `${name[randomNum(0, name.length - 1)]}${roomNameAppendix[randomNum(0, roomNameAppendix.length - 1)]}`;
+
+const randomRating = () => (Math.random() * (5.0 - 1.0) + 1.0).toFixed(1);
 
 const wstream1 = fs.createWriteStream('noSQLBookingData.csv');
 
-wstream1.write('id,roomname,price,cleaning_fee,service_fee,tax,max_adults,max_child,max_infants,min_night,max_night,rating,num_reviews,bookings\n');
-
-for (let i = 1; i <= 1000000; i += 1) {
-  wstream1.write(
-    i + ',' +
-    faker.name.findName() + ' ' + roomNameAppendix[randomNum(0, roomNameAppendix.length - 1)] + ', ' +
-    randomNum(50, 200) + ', ' +
-    5 + ', ' +
-    5 + ', ' +
-    10 + ', ' +
-    randomNum(1, 6) + ', ' +
-    randomNum(0, 4) + ', ' +
-    randomNum(0, 2) + ', ' +
-    randomNum(50, 100) + ', ' +
-    randomNum(100, 500) + ', ' +
-    (Math.random() * (5.0 - 1.0) + 1.0).toFixed(1) + ', ' +
-    randomNum(0, 100) + ',"{'
-  );
+const bookGenerator = (counter) => {
+  const email = ['tom@gmail', 'sam@yahoo.com', 'bill@aol.com', 'liz@hotmail.com', 'ashley@gmail.com', 'michael@yahoo.com', 'pam@gmail.com'];
+  const day = 86400000;
   let startDate = moment().toDate().getTime();
-  const num = randomNum(5, 10);
+  const newObj = {};
+
+  const num = randomNum(5, 8);
   for (let j = 1; j <= num; j += 1) {
     const chkIn = startDate + (day * randomNum(0, 5));
     const chkOut = chkIn + (day * randomNum(2, 5));
-    wstream1.write(
-      "{email:'" + faker.internet.email() + "', " +
-        "guest_adults:" + randomNum(1, 3) + ", " +
-        "guest_child:" + randomNum(0, 2) + ", " +
-        "guest_infant:" + randomNum(0, 1) + ", " +
-        "check_in:'" + new Date(chkIn) + "', " +
-        "check_out:'" + new Date(chkOut) + "', "
-    );
-    if (j === num) {
-      wstream1.write('roomid: ' + i + '}');
-    } else {
-      wstream1.write('roomid: ' + i + '}, ');
-    }
+
+    const obj = {
+      email: `${email[randomNum(0, email.length - 1)]}`,
+      gueset_adults: `${randomNum(1, 3)}`,
+      guest_child: `${randomNum(1, 3)}`,
+      guest_infant: `${randomNum(1, 3)}`,
+      check_out: `${new Date(chkOut)}`,
+      roomid: `${counter}`,
+    };
+
     const gap = (day * randomNum(2, 4));
     startDate = chkOut + gap;
+
+    newObj[`${new Date(chkIn)}`] = obj;
   }
-  wstream1.write('}"\n');
-  if (i % 100000 === 0) {
-    console.log(i);
+  return JSON.stringify(newObj);
+};
+
+async function dataGenerator() {
+  wstream1.write('id|roomname|price|cleaning_fee|service_fee|tax|max_adults|max_child|max_infants|min_night|max_night|rating|num_reviews|bookings\n');
+
+  for (let i = 1; i <= 10000000; i += 1) {
+    const writeRoom = wstream1.write(
+      `${i}|${randomName()}|${randomNum(50, 200)}|${5}|${5}|${10}|${randomNum(1, 6)}|${randomNum(0, 4)}|${randomNum(0, 2)}|${randomNum(50, 100)}|${randomNum(100, 500)}|${randomRating()}|${randomNum(0, 100)}| ${(bookGenerator(i))}\n`
+    );
+    if (i % 100000 === 0) {
+      console.log(i);
+    }
+    if (!writeRoom) {
+      await new Promise((resolve) => {
+        wstream1.once('drain', resolve);
+      });
+    }
   }
+
+  wstream1.end();
+  console.log('Successfull Seed!');
 }
-wstream1.end();
-console.log('Successfull Seed!');
+
+dataGenerator();
